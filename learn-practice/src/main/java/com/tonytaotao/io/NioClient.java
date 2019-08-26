60ec087a23ab11e506c74a0c931a088c4d1fd957
wujintao <jintao.wu@sf-express.com>
2019-08-26 19:49:40 +0800

    init

--- learn-practice/src/main/java/com/tonytaotao/io/NioClient.java
+++ learn-practice/src/main/java/com/tonytaotao/io/NioClient.java
@@ -1,203 +1,131 @@
 package com.tonytaotao.io;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.Date;
+import java.util.Set;
 
 public class NioClient {
 
+    /**
+     * 发送数据缓冲区
+     * */
+    private static ByteBuffer sBuffer = ByteBuffer.allocate(1024);
+    /**
+     * 接受数据缓冲区
+     * */
+    private static ByteBuffer rBuffer = ByteBuffer.allocate(1024);
 
-    private static CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
-    private static CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
+    private static Selector selector;
 
-
-
-    public static void main(String[] args) {
-
-        String name = "";
-
-        Client client1 = new Client("A", decoder, encoder);
-        new Thread(client1).start();
-
-        Scanner scanner = new Scanner(System.in);
+    public  void init() {
         try {
-
-            while (scanner.hasNextLine()) {
-                String readLine = scanner.nextLine();
-                if ("".equals(readLine)) {
-                    continue;
-                }
-
-                if ("bye".equals(readLine)) {
-                    client1.close();
-                }
-
-                if ("".equals(name)) {
-                    name = readLine;
-                    readLine = name + "#@#";
-                } else {
-                    readLine = name + "#@#" + readLine;
-                }
-
-                client1.send(readLine);
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-
-
-class Client implements Runnable {
-
-    private String clientName;
-    private CharsetDecoder decoder;
-    private CharsetEncoder encoder;
-
-    private ByteBuffer byteBuffer;
-    private Selector selector = null;
-    private SocketChannel socketChannel = null;
-    private SelectionKey selectionKey = null;
-
-    private boolean flag = true;
-
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
-
-    public Client(String clientName, CharsetDecoder decoder, CharsetEncoder encoder) {
-
-        this.clientName = clientName;
-        this.decoder = decoder;
-        this.encoder = encoder;
-
-        try {
-
-            byteBuffer = ByteBuffer.allocate(1024);
             selector = Selector.open();
+
             SocketChannel socketChannel = SocketChannel.open();
             socketChannel.configureBlocking(false);
             socketChannel.connect(new InetSocketAddress("127.0.0.1", 8080));
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
 
-            selectionKey = socketChannel.register(selector, SelectionKey.OP_CONNECT);
+            System.out.println("客户端启动成功");
 
-        } catch (Exception e) {
+            while (true) {
+                selector.select();
+                Set<SelectionKey> keySet = selector.selectedKeys();
+                for (final SelectionKey key: keySet) {
+                    handle(key);
+                }
+                keySet.clear();
+            }
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
-    @Override
-    public void run() {
-        System.out.println("客户端" + clientName + "正在运行");
+    private void handle(SelectionKey key) throws IOException{
 
-        try {
+        if (key.isConnectable()) {
 
-            while (this.flag) {
-                int num = 0;
-                try {
-                    num = selector.select();
-                } catch (Exception e) {
+            SocketChannel client = (SocketChannel) key.channel();
+            if (client.isConnectionPending()) {
+                client.finishConnect();
+                System.out.println("客户端连接服务器成功");
 
-                }
-                if (num > 0) {
-                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                    while (iterator.hasNext()) {
-                        SelectionKey key = iterator.next();
-                        iterator.remove();
+                sBuffer.clear();
+                sBuffer.put((new Date().toString() + "已连接！").getBytes());
+                sBuffer.flip();
 
-                        if (key.isConnectable()) {
+                client.write(sBuffer);
 
-                            SocketChannel client = (SocketChannel) key.channel();
-                            if (client.isConnectionPending()) {
-                                client.finishConnect();
-                            }
-                            client.configureBlocking(false);
-                            client.register(selector, SelectionKey.OP_READ);
 
-                        } else if (key.isReadable()) {
-
+                new Thread() {
+                    @Override
+                    public void run() {
+                        while (true) {
                             try {
-                                SocketChannel channel = (SocketChannel) key.channel();
-                                int readNum = 0;
-                                String msg = "";
-                                while ((readNum = channel.read(byteBuffer)) > 0) {
-                                    byteBuffer.flip();
-                                    msg = msg + decoder.decode(byteBuffer).toString();
 
-                                }
+                                InputStreamReader input = new InputStreamReader(System.in);
+                                BufferedReader br = new BufferedReader(input);
+                                String sendText = br.readLine();
 
-                                key.interestOps(SelectionKey.OP_READ);
+                                sBuffer.clear();
+                                sBuffer.put(sendText.getBytes());
+                                sBuffer.flip();
 
-                                if ("user-repeat".equals(msg)) {
-                                    System.out.println("用户已存在");
-                                }
-
-                                if (!"".equals(msg)) {
-                                    System.out.println("客户端" + clientName + "收到服务器消息:" + msg);
-                                }
-
+                                client.write(sBuffer);
 
                             } catch (IOException e) {
                                 e.printStackTrace();
+                                break;
                             }
 
+
                         }
-                    }
-                }
+                     }
+                }.start();
+
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
+            client.configureBlocking(false);
+            client.register(selector, SelectionKey.OP_READ);
 
-        } finally {
+        } else if (key.isReadable()) {
 
-            if (this.socketChannel != null && this.socketChannel.isOpen()) {
-                try {
-                    this.socketChannel.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            try {
+                SocketChannel client = (SocketChannel) key.channel();
+
+                rBuffer.clear();
+
+                int readNum = 0;
+                if ((readNum = client.read(rBuffer)) > 0) {
+                    String receiveText = new String(rBuffer.array(), 0, readNum);
+
+                    System.out.println("客户端收到服务器消息：" + receiveText);
+
+                    client = (SocketChannel) key.channel();
+                    client.register(selector, SelectionKey.OP_READ);
                 }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            if (this.selector != null && this.selector.isOpen()) {
-                try {
-                    this.selector.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
-
 
     }
 
-    public void send(String msg) {
-        try {
-            SocketChannel channel = (SocketChannel) selectionKey.channel();
-            channel.write(encoder.encode(CharBuffer.wrap(msg)));
-        } catch (Exception e) {
-            System.out.println("客户端" + clientName + "发送消息失败, 消息为:" + msg);
-            e.printStackTrace();
-        }
-    }
+    public static void main(String[] args) {
 
-    public void close() {
-        try {
-            selector.close();
-            socketChannel.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new NioClient().init();
+
     }
 }
+
--- learn-practice/src/main/java/com/tonytaotao/io/NioServer.java
+++ learn-practice/src/main/java/com/tonytaotao/io/NioServer.java
@@ -3,181 +3,107 @@ package com.tonytaotao.io;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.channels.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class NioServer {
 
-    public static HashSet<String> userList = new HashSet<>();
+    /*接受数据缓冲区*/
+    private static ByteBuffer sBuffer = ByteBuffer.allocate(1024);
+    /*发送数据缓冲区*/
+    private static ByteBuffer rBuffer = ByteBuffer.allocate(1024);
 
+    /*映射客户端channel */
+    private Map<String, SocketChannel> clientsMap = new HashMap<String, SocketChannel>();
 
-    public static void main(String[] args) {
-        new Thread(new Server()).start();
-    }
+    private static Selector selector;
 
-}
-
-class Server implements Runnable {
-
-    private static CharsetEncoder encoder = Charset.forName("UTF-8").newEncoder();
-    private static CharsetDecoder decoder = Charset.forName("UTF-8").newDecoder();
-
-    private boolean flag = true;
-
-    private Selector selector = null;
-    private ServerSocketChannel serverSocketChannel = null;
-
-    private List<SocketChannel> clientList = null;
-
-    public Server() {
-
+    public void init() {
         try {
+
             selector = Selector.open();
 
-            serverSocketChannel = ServerSocketChannel.open();
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.configureBlocking(false);
             serverSocketChannel.socket().bind(new InetSocketAddress(8080));
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-            clientList = new ArrayList<>();
-
             System.out.println("服务器初始化完毕，开始接受客户端连接");
+
+            while (true) {
+                try {
+
+                    selector.select();
+
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    for (SelectionKey key : selectionKeys) {
+                        handle(key);
+                    }
+
+                    selectionKeys.clear();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void setFlag(boolean flag) {
-        this.flag = flag;
-    }
 
-    @Override
-    public void run() {
+    private void handle(SelectionKey key) throws IOException {
 
-        while (this.flag) {
 
-            int num = 0;
-            try {
-                num = selector.select();
-            } catch (IOException e) {
-                System.out.println("服务器选择器轮询出错");
-                e.printStackTrace();
+        if (key.isAcceptable()) {
+
+            ServerSocketChannel server = (ServerSocketChannel) key.channel();
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+            client.register(selector, SelectionKey.OP_READ);
+
+            System.out.println("客户端" + client.getRemoteAddress() + "已成功建立连接！");
+
+        } else if (key.isReadable()) {
+            SocketChannel client = (SocketChannel) key.channel();
+
+            rBuffer.clear();
+
+            if (client.read(rBuffer) > 0) {
+                rBuffer.flip();
+
+                String receiveText = String.valueOf(Charset.forName("UTF-8").decode(rBuffer).array());
+                System.out.println("服务器收到消息：" + receiveText);
+
+                broadCast(client, "服务器已收到消息");
+
+                client = (SocketChannel) key.channel();
+                client.register(selector, SelectionKey.OP_READ);
             }
 
-            if (num > 0) {
-                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                while (iterator.hasNext()) {
-                    SelectionKey key = iterator.next();
-                    iterator.remove();
-
-                    if (key.isAcceptable()) {
-                        SocketChannel client = null;
-                        try {
-                            ServerSocketChannel server = (ServerSocketChannel) key.channel();
-                            client = server.accept();
-                            client.configureBlocking(false);
-                            client.register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE);
-
-                            key.interestOps(SelectionKey.OP_ACCEPT);
-
-                            System.out.println("客户端" + client.getRemoteAddress() + "已成功建立连接！");
-
-                            client.write(encoder.encode(CharBuffer.wrap("您已连接服务器,请输入你的名字")));
-
-                            clientList.add(client);
-
-                        } catch (IOException e) {
-
-                            System.out.println("服务器配置客户端连接出错");
-                            if (client != null) {
-                                try {
-                                    client.close();
-                                } catch (IOException e1) {
-                                    e1.printStackTrace();
-                                }
-                            }
-                        }
-
-                    } else if (key.isReadable()) {
-                        SocketChannel client = (SocketChannel) key.channel();
-                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-
-                        StringBuilder content = new StringBuilder();
-                        try {
-                            while (client.read(byteBuffer) > 0) {
-                                byteBuffer.flip();
-                                content.append(decoder.decode(byteBuffer));
-                            }
-
-                            key.interestOps(SelectionKey.OP_READ);
-
-                            if (content.length() > 0) {
-                                System.out.println("服务器收到客户端" + client.getRemoteAddress() +"数据:" + content);
-
-                                String[] arrayContent = content.toString().split("#@#");
-                                try {
-
-                                    // 注册
-                                    if (arrayContent != null && arrayContent.length == 1) {
-                                        String name = arrayContent[0];
-                                        if (NioServer.userList.contains(name)) {
-                                            client.write(encoder.encode(CharBuffer.wrap("user-repeat")));
-                                        } else {
-                                            NioServer.userList.add(name);
-                                            String msg = "欢迎" + name + "加入群聊";
-                                            broadCast(selector, client, msg);
-                                        }
-                                    } else if (arrayContent != null && arrayContent.length >= 1) {
-                                        // 发送消息
-                                        String name = arrayContent[0];
-                                        String msg = name + "说:" + arrayContent[1];
-                                        broadCast(selector, client, msg);
-                                    }
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        } catch (IOException e) {
-                            try {
-                                System.out.println("客户端" + client.getRemoteAddress() + "已断开连接");
-                                client.close();
-                            } catch (IOException e1) {
-                                e1.printStackTrace();
-                            }
-
-                            int length = clientList.size();
-                            for (int i = 0 ; i< length ; i++) {
-                                if (clientList.get(i).equals(client)) {
-                                    clientList.remove(i);
-                                    break;
-                                }
-                            }
-                        }
-
-                    }
-                }
-            }
         }
 
     }
 
-    public static void broadCast(Selector selector, SocketChannel ownerClient, String content) throws Exception{
-        for (SelectionKey key : selector.keys()) {
-            Channel channel = key.channel();
-            if (channel instanceof SocketChannel) {
-                SocketChannel client = (SocketChannel) channel;
-                client.write(encoder.encode(CharBuffer.wrap(content)));
-            }
+    private void broadCast (SocketChannel client,String msg) throws IOException {
+        sBuffer.clear();
+        sBuffer.put(msg.getBytes());
+        sBuffer.flip();
 
-
-        }
+        client.write(sBuffer);
     }
+
+    public static void main(String[] args) {
+        new NioServer().init();
+    }
+
 }
+
