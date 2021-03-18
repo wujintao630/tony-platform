e3654a81c16de2b8fd62d153cfc23d767c2437c6
wujintao <wujintao@ybm100.com>
2021-03-18 14:55:54 +0800

    学习

--- tony-rpc/tony-rpc-core/src/main/java/com/tonytaotao/rpc/core/provider/Provider.java
+++
@@ -1,7 +0,0 @@
-package com.tonytaotao.rpc.core.provider;
-
-import com.tonytaotao.rpc.core.Caller;
-
-public interface Provider<T> extends Caller<T> {
-
-}
--- tony-rpc/tony-rpc-core/src/main/java/com/tonytaotao/rpc/netty/server/NettyServer.java
+++
@@ -1,7 +0,0 @@
-package com.tonytaotao.rpc.netty.server;
-
-import com.tonytaotao.rpc.netty.Endpoint;
-
-public interface NettyServer extends Endpoint {
-
-}
